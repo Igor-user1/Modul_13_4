a45dd715622277abdf9f64e875e45345a4991431
Igor-user1 <xadeev.igor@icloud.com>
2024-12-02 10:15:23 +0300

    Update modul_13_2.py

--- modul_13_2.py
+++ modul_13_2.py
@@ -16,23 +16,28 @@ class UserState(StatesGroup):
     weight = State()
 
 
+@dp.message_handler(text=['/start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
 @dp.message_handler(text="Calories")
 async def set_age(message):
-    await message.answer('Введите свой возраст')
+    await message.answer('Введите свой возраст:')
     await UserState.age.set()
 
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
     await state.update_data(first=message.text)
-    await message.answer('Введите свой рост')
+    await message.answer('Введите свой рост:')
     await UserState.growth.set()
 
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
     await state.update_data(second=message.text)
-    await message.answer('Введите свой вес')
+    await message.answer('Введите свой вес:')
     await UserState.weight.set()
 
 
@@ -40,10 +45,16 @@ async def set_weight(message, state):
 async def send_calories(message, state):
     await state.update_data(third=message.text)
     data = await state.get_data()
-    await message.answer(f'Ваша норма калорий -'
-                         f'{int(data["first"])*10 + int(data["second"])*6.25 - int(data["third"])*5 + 5}')
+    await message.answer(f'Ваша норма калорий '
+                         f'{int(data["third"])*10 + int(data["second"])*6.25 - int(data["first"])*5 + 5}')
     await state.finish()
+
+
+@dp.message_handler()
+async def start(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
